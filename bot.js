23a7a2fb0c98366dc2fdebbb5aeb5fa0845f7371
MichaelVenturi <michaelventuri58@aol.com>
2022-01-01 21:22:03 -0500

    randomly shouts out, user can change shoutout frequency

--- bot.js
+++ bot.js
@@ -26,10 +26,8 @@ fs.readdirSync("./Commands").filter((file) =>
 );
 
 const prefix = "!";
-//const image = new MessageAttachment("./images/Christina-Applegate.jpg");
-const embed = new MessageEmbed()
-  .setTitle("title")
-  .setThumbnail("attachment://Christina-Applegate.jpg");
+
+let frequency = 5;
 
 client.on("ready", () => {
   console.log("bot online");
@@ -39,17 +37,34 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
+  // ignore if its a DM to the bot
   if (message.author.bot || message.channel.type == "DM") return;
-  if (!message.content.startsWith(process.env.PREFIX)) return;
-  const args = message.content.substring(process.env.PREFIX.length).split(/ +/);
-  console.log(args);
-  switch (args[0].toLowerCase()) {
-    case "shoutout":
-      //const image = new MessageAttachment("./images/Christina-Applegate.jpg");
+  // Only respond to the bot's command prefix
+  if (!message.content.startsWith(process.env.PREFIX)) {
+    if (Math.floor(Math.random() * frequency) + 1 == frequency) {
       message.channel.send({
         content: "Let's give a quick shoutout to Christina Applegate!",
         files: ["./images/Christina-Applegate.jpg"],
       });
+    }
+  }
+  const args = message.content.substring(process.env.PREFIX.length).split(/ +/);
+  console.log(args);
+  switch (args[0].toLowerCase()) {
+    case "shoutout":
+      message.channel.send({
+        content: "Let's give a quick shoutout to Christina Applegate!",
+        files: ["./images/Christina-Applegate.jpg"],
+      });
+      break;
+    case "setf":
+      //console.log(parseInt(args[1]));
+      if (!args[1]) {
+        message.channel.send("invalid");
+      } else {
+        frequency = parseInt(args[1]);
+        message.channel.send(`shoutout frequency set to 1 / ${frequency}`);
+      }
   }
 });
 
