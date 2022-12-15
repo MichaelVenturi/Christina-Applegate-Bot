ed98004416fb1dd4c3908a9a117e8821dc188469
MichaelVenturi <michaelventuri58@aol.com>
2022-12-15 14:27:22 -0500

    defined shoutout and setf execution functions

--- Commands/setFrequency.js
+++ Commands/setFrequency.js
@@ -3,8 +3,17 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("set_frequency")
-    .setDescription("Sets how often random shoutouts are given"),
+    .setDescription("Sets how often random shoutouts are given")
+    .addIntegerOption((option) =>
+      option
+        .setName("input")
+        .setDescription("Set frequency of random shoutouts")
+        .setMinValue(0)
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    await interaction.reply("freq");
+    const freq = interaction.options.getInteger("input");
+    interaction.client.shoutoutFreq = freq;
+    await interaction.reply(`shoutout frequency set to 1 / ${freq}`);
   },
 };
--- Commands/shoutout.js
+++ Commands/shoutout.js
@@ -5,6 +5,9 @@ module.exports = {
     .setName("shoutout")
     .setDescription("Gives a shoutout to Christina Applegate"),
   async execute(interaction) {
-    await interaction.reply("test");
+    await interaction.reply({
+      content: "Let's give a quick shoutout to Christina Applegate!",
+      files: ["./images/Christina-Applegate.jpg"],
+    });
   },
 };
