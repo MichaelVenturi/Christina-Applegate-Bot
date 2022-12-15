09ee93d2a80d4b74ed3af083f2f9e4b7d3162072
MichaelVenturi <michaelventuri58@aol.com>
2022-12-15 14:09:12 -0500

    setup command handling and registration

--- bot.js
+++ bot.js
@@ -41,12 +41,27 @@ client.on("ready", () => {
   });
 });
 
-client.on("messageCreate", async (message) => {
-  LegacyShoutout(client, message);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    await interaction.reply({
+      content: "There was an error while executing this command",
+      ephemeral: true,
+    });
+  }
 });
 
-client.on("interactionCreate", async (interaction) => {
-  console.log(interaction);
+client.on("messageCreate", async (message) => {
+  LegacyShoutout(client, message);
 });
 
 client.login(process.env.TOKEN);
